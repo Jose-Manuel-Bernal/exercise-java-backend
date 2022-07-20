d13521a073b3411fe86042249ab915fbf97e5ae7
Jose Bernal Aguilar <jose.bernal@sofka.local>
2022-07-19 19:10:45 -0500

    project created

---
+++ src/main/java/com/sofka/exercise/java_backend/tournament/TournamentApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.exercise.java_backend.tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentApplication.class, args);
+	}
+
+}
