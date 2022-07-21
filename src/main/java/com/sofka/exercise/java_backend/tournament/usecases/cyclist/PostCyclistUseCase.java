1d95fd29e0d13a57f5caeb3d8b2b7bf19157c8d7
Jose Manuel Bernal Aguilar <josem.bernal@udea.edu.co>
2022-07-21 14:07:03 -0500

    validation to create a new Cyclist checking that the teamcode exist

--- src/main/java/com/sofka/exercise/java_backend/tournament/usecases/cyclist/PostCyclistUseCase.java
+++ src/main/java/com/sofka/exercise/java_backend/tournament/usecases/cyclist/PostCyclistUseCase.java
@@ -1,13 +1,13 @@
 package com.sofka.exercise.java_backend.tournament.usecases.cyclist;
 
 import com.sofka.exercise.java_backend.tournament.dto.CyclistDTO;
-import com.sofka.exercise.java_backend.tournament.entity.Cyclist;
+import com.sofka.exercise.java_backend.tournament.entity.Team;
 import com.sofka.exercise.java_backend.tournament.mapper.CyclistMapper;
+import com.sofka.exercise.java_backend.tournament.mapper.TeamMapper;
 import com.sofka.exercise.java_backend.tournament.repository.CyclistRepository;
-import lombok.RequiredArgsConstructor;
+import com.sofka.exercise.java_backend.tournament.repository.TeamRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
@@ -15,22 +15,32 @@ import javax.validation.Valid;
 @Service
 @Validated
 public class PostCyclistUseCase {
-    private final CyclistRepository repository;
-    private final CyclistMapper mapper;
+    private final CyclistRepository cyclistRepository;
+    private final TeamRepository teamRepository;
+    private final CyclistMapper cyclistMapper;
 
-    public PostCyclistUseCase(CyclistRepository repository, CyclistMapper mapper) {
-        this.mapper = mapper;
-        this.repository = repository;
+
+    public PostCyclistUseCase(CyclistRepository cyclistRepository, TeamRepository teamRepository, CyclistMapper cyclistMapper) {
+        this.cyclistRepository = cyclistRepository;
+        this.teamRepository = teamRepository;
+        this.cyclistMapper = cyclistMapper;
     }
 
     public Mono<CyclistDTO> apply(@Valid CyclistDTO cyclistDTO) {
         Integer cyclistNum = cyclistDTO.getNumber();
-        return repository.existsByNumber(cyclistNum).flatMap(aBoolean -> {
+        return cyclistRepository.existsByNumber(cyclistNum).flatMap(aBoolean -> {
             if (Boolean.TRUE.equals(aBoolean)){
                 return Mono.error(() -> new Exception("the number has already been taken"));
             }
-            return repository.save(mapper.toCyclistEntity(cyclistDTO))
-                    .map(cyclist -> mapper.toCyclistDTO(cyclist));
+        return teamRepository.existsByTeamCode(cyclistDTO.getTeamCode()).flatMap(aBoolean1 -> {
+            if (Boolean.TRUE.equals(aBoolean1)) {
+                    return cyclistRepository.save(cyclistMapper.toCyclistEntity(cyclistDTO))
+                            .map(cyclist -> cyclistMapper.toCyclistDTO(cyclist));
+            }
+            return Mono.error(() -> new Exception("the team code does not exist"));
+            });
         });
     }
+
+    
 }
